dbf167575f495b7e24ea324ec3b2ff66b767c893
madasilva1 <madasilva195@gmail.com>
2019-09-06 12:02:58 -0400

    COD has been implemented

---
+++ COD.java
@@ -0,0 +1,12 @@
+public class COD extends BankAccount {
+    public void SetAccount(String account){
+        this.account = account;
+    }
+    public String GetAccount(){ return  account;}
+
+    public void SetBalance(int balance){ this.balance = balance; }
+    public int GetBalance(){ return  balance;}
+
+
+
+}
