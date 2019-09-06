1a89145e5ad93aa746a098d24091e23521999e13
madasilva1 <madasilva195@gmail.com>
2019-09-06 12:00:29 -0400

    BankAccount class bas been implemented

---
+++ BankAccount.java
@@ -0,0 +1,14 @@
+public class BankAccount {
+    String account;
+    int balance;
+    public void SetAccount(String account){
+        this.account = account;
+    }
+    public String GetAccount(){ return  account;}
+
+    public int GetBalance(){ return  balance;}
+    public void SetBalance(int balance){
+        this.balance = balance;
+    }
+
+}
