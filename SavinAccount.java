85025da3268dc488ee122a07a965cf36bbf92001
madasilva1 <madasilva195@gmail.com>
2019-09-06 12:03:48 -0400

    SavinAccount has been implemented

---
+++ SavinAccount.java
@@ -0,0 +1,8 @@
+public class SavinAccount extends BankAccount {
+      public void SetAccount(String account){
+          this.account= account;
+      }
+      public String GetAccount(){
+          return account;
+      }
+}
